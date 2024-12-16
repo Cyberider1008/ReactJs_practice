7bec592cc890ba62f881943d916753cdab75ce79
Cyberider1008 <abaranwal.it@gmail.com>
2024-12-16 22:14:58 +0530

    Starting ReactJS

---
+++ firstviteproject/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+console.log("hiiiiii");
+  return (
+    <>
+    <h1>Hi!!! I am Cyberider1008. This is my First Project</h1>
+    </>
+  )
+}
+
+export default App
