20e14d77b953e78b1a4969e317b358e3594198f4
Cyberider1008 <abaranwal.it@gmail.com>
2024-12-17 13:33:58 +0530

    something change

--- firstviteproject/src/App.jsx
+++ firstviteproject/src/App.jsx
@@ -1,10 +1,48 @@
 import { useState } from 'react'
 
 function App() {
-console.log("hiiiiii");
+// let counter = 10;
+
+let [counter, setcounter] = useState(10);
+
+const btn = document.querySelectorAll("button");
+
+let add = ()=>{
+  if(counter < 20){
+    btn[1].disabled = false;
+   
+    counter = counter+1;
+  setcounter(counter);
+  console.log(counter);
+
+  }
+  else{
+    
+    btn[0].disabled = true;
+    
+  }
+  }
+
+  let minus = ()=>{
+    if(counter > 0 ){
+       btn[0].disabled = false;
+      counter --;
+      setcounter(counter);
+
+    }
+    else{
+      btn[1].disabled = true;
+    }
+  }
+
+
   return (
     <>
-    <h1>Hi!!! I am Cyberider1008. This is my First Project</h1>
+
+    <h1>Counter : {counter}</h1>
+    <button onClick={add}>Add</button>
+    <button onClick= {minus}>Minus</button>
+    <footer>Here Your Counter is {counter}</footer>
     </>
   )
 }
---
+++ firstviteproject/src/Print.jsx
@@ -0,0 +1,12 @@
+function Print(){
+    let name = "ashishkumar";
+    return (
+        <>
+        
+        <h1>Print here {name}</h1>
+        
+        </>
+
+    )
+}
+export default Print
